a09bb44b056602794a33355c2954b18e156090d2
陈杰 <jiegou04@gmail.com>
2020-05-08 16:28:36 +0800

    修改序列快捷键例子，输出序列快捷键文本，代替原来简单换行

--- examples/ConsoleHook/DetectSequences.cs
+++ examples/ConsoleHook/DetectSequences.cs
@@ -1,11 +1,11 @@
-﻿// This code is distributed under MIT license. 
+﻿// This code is distributed under MIT license.
 // Copyright (c) 2010-2018 George Mamaladze
 // See license.txt or https://mit-license.org/
 
+using Gma.System.MouseKeyHook;
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using Gma.System.MouseKeyHook;
 
 namespace ConsoleHook
 {
@@ -13,16 +13,22 @@ namespace ConsoleHook
     {
         public static void Do(Action quit)
         {
+            Action writeLine = () => Console.WriteLine("Control+Z,B序列快捷");
             var map = new Dictionary<Sequence, Action>
             {
-                {Sequence.FromString("Control+Z,B"), Console.WriteLine},
-                {Sequence.FromString("Control+Z,Z"), Console.WriteLine},
+                {Sequence.FromString("Control+Z,B"), new Action(() =>
+                            Console.WriteLine("Control+Z,B序列快捷"))},
+                {Sequence.FromString("Control+Z,Z"), new Action(() =>
+                            Console.WriteLine("Control+Z,Z序列快捷"))},
                 {Sequence.FromString("Escape,Escape,Escape"), quit}
             };
 
             Console.WriteLine("Detecting following combinations:");
             foreach (var sequence in map.Keys)
+            {
                 Console.WriteLine("\t{0}", sequence);
+            }
+
             Console.WriteLine("Press 3 x ESC (three times) to exit.");
 
             //Actual loop
